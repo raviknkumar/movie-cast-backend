c3c82f98143c1fcb82c27f5bebe2f0b923886549
iamravi <ravikumar.k@ninjacart.com>
2020-07-14 18:23:21 +0530

    Server Setup

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const { getCurrentTime } = require('./utils/datetimeutils');
 const formatMessage = require('./utils/messages');
 const { userJoin } = require('./utils/users');
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 // server for socket io
 const server = http.createServer(app);
 
